ba609c57baaba1a5ef35c035ec67e02292cbad10
aiyoshida <ai2150sorairo@gmail.com>
2020-07-19 20:53:44 +0900

    データ型・文字列クラス

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,23 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+        
+        int hogeInt;
+         hogeInt = 50;
+
+        String hogeString; 
+        hogeString = "テスト";
+
+        double hogeDouble = 0.2;
+        //double型のhogeDoubleという変数を宣言し、値「0.2」で初期化
+
+        hogeInt = 100;
+
+        hogeString = "合格";
+
+        System.out.println(hogeInt);
+        // hogeStringの値を表示する
+        System.out.println(hogeDouble);
+
+    }
+}
