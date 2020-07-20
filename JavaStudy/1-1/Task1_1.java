821788b9d313fed2b053eb729aceac21d8170bbd
aiyoshida <ai2150sorairo@gmail.com>
2020-07-21 01:11:08 +0900

    データ型・文字クラス　再提出

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -1,21 +1,30 @@
 public class Task1_1 {
-
     public static void main(String[] args) {
-        
-        int hogeInt = 50;
-        
+      // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+      int hogeInt = 50;
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
         String hogeString = "テスト";
 
-         double hogeDouble = 0.2;
-         // double型のhogeDoubleという変数を宣言し、値「0.2」で初期化
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [double型の変数hogeDoubleを設定して0.2に初期化]
+        double hogeDouble = 0.2;
 
-         hogeInt = 100;
+        // ④変数 hogeInt を値「100」で上書き
+        hogeInt = 100;
 
-         hogeString = "合格";
+        // ⑤変数 hogeString を値「合格」で上書き
+        hogeString = "合格";
 
-         System.out.println(hogeInt);
-        // hogeStringの値を表示する
+        // ⑥変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // [hogeStringを表示]
+        System.out.println(hogeString);
+        // ⑧変数 hogeDouble の値を表示
         System.out.println(hogeDouble);
-
+        
+  
     }
 }
