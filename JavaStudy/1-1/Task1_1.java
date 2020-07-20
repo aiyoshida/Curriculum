e48fe60c390be6a0a1b561c56e207115f4bc3ef2
aiyoshida <ai2150sorairo@gmail.com>
2020-07-20 13:30:59 +0900

    Task1_1再提出

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -2,20 +2,18 @@ public class Task1_1 {
 
     public static void main(String[] args) {
         
-        int hogeInt;
-         hogeInt = 50;
+        int hogeInt = 50;
+        
+        String hogeString = "テスト";
 
-        String hogeString; 
-        hogeString = "テスト";
+         double hogeDouble = 0.2;
+         // double型のhogeDoubleという変数を宣言し、値「0.2」で初期化
 
-        double hogeDouble = 0.2;
-        //double型のhogeDoubleという変数を宣言し、値「0.2」で初期化
+         hogeInt = 100;
 
-        hogeInt = 100;
+         hogeString = "合格";
 
-        hogeString = "合格";
-
-        System.out.println(hogeInt);
+         System.out.println(hogeInt);
         // hogeStringの値を表示する
         System.out.println(hogeDouble);
 
