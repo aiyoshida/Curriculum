8e30b62f464b653be11ad0e21bc73d32059cd637
aiyoshida <ai2150sorairo@gmail.com>
2020-07-22 15:22:22 +0900

    チェックテスト　提出

---
+++ JavaStudy/.vscode/Check1/Check.java
@@ -0,0 +1,36 @@
+public class Check {
+    public static void main(String[] args) {
+        
+        // 問1
+        // int型の配列dataを作成し、値を3,1,2,7,5で初期化しなさい
+        int [] data = {3,1,2,7,5};
+    
+        
+        /*
+        * 問2
+        * 以下のfor文を完成させなさい
+        */
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        
+        //バブルソート
+        for (int i = 0; i < data.length; i++) { //31275
+            for (int j = data.length - 1 ; j > i; j--) {
+                /* 
+                * 問3
+                * 以下、配列の添字を入れてソートを完成させなさい
+                */
+                if(data[i] > data[j]){
+                  int box = data[i];
+                  data[i] = data[j];
+                  data[j] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+    }
+}
